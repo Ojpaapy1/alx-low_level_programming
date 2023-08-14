92b3fc719cdc317ec8bdf381c7deaa2a59f8ad89
Ojpaapy1 <avanjude4@gmail.com>
2023-08-14 04:32:12 -0700

    lowercase then newline

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -printing all letters
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int lowercase = 'a';
+while (lowercase <= 'z')
+{
+if (lowercase == 'e' || lowercase == 'q')
+{
+lowercase += 1;
+}
+else
+{
+putchar (lowercase);
+lowercase += 1;
+}
+}
+putchar ('\n');
+return (0);
+}
+
