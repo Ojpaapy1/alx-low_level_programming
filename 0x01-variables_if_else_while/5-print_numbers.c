c8cb1f632e87fc822634fc1372de63178487bd93
Ojpaapy1 <avanjude4@gmail.com>
2023-08-14 04:34:27 -0700

    print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print single no base 10
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+printf("\n");
+return (0);
+}
