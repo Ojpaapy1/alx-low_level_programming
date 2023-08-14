cf324edb6a7762ea7d20a519f4eadbb7b1a5003e
Ojpaapy1 <avanjude4@gmail.com>
2023-08-14 03:38:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -using puts for printing
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
